3fc01c00cc30c8de1cd39d21afe1f0b1e2babd30
ansi7794 <anushree@bu.edu>
2018-10-23 14:56:09 -0400

    exercise2

---
+++ Mugiwara-Level1/Exercise2.go
@@ -0,0 +1,28 @@
+// Question
+// 1. Use var to declare three variables. The variables should
+// have package level scope. Do not assign values to the variables.
+// Use the following identifiers for the variables and make
+// sure the variables store values of the following type
+// a. identifier "x" type int
+// b. identifier "y" type string
+// c. identifier "z" type bool
+// 2. in func main
+// a. print out the values for each identifier
+// b. the compiler assigned values to the variables.
+// What are these values called?
+
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println("x =", x)
+	fmt.Println("y =", y)
+	fmt.Println("z =", z)
+
+	// 2b. zero values.
+}
