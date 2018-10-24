fcfec71c7491d069b23a61a6c07d49b51a8f3f5f
ansi7794 <anushree@bu.edu>
2018-10-24 01:23:47 -0400

    exercise 3

---
+++ Mugiwara-Level2/Exercise3.go
@@ -0,0 +1,15 @@
+// create a typed and untyped constants
+// print the values of the constants
+
+package main
+
+import "fmt"
+
+const(
+	a int = 5
+	b = "string"
+)
+
+func main() {
+	fmt.Println(a, b)
+}
