524f9362002c5164384405407d2aa903a8f2dd75
ansi7794 <anushree@bu.edu>
2018-10-25 03:09:30 -0400

    if

--- 009-ifcondition.go
+++ 009-ifcondition.go
@@ -1,3 +1,13 @@
 package main
 
+import "fmt"
 
+func main() {
+ 	if x := 42; x == 40 {
+ 		fmt.Println("x is 40")
+	} else if x == 41 {
+		fmt.Println("x is 41")
+	} else {
+		fmt.Println("x is 42")
+	}
+ }
