b0978d9a1274119e3471aac83602440127a26a6b
ansi7794 <anushree@bu.edu>
2018-10-30 00:42:22 -0400

    exercise2

---
+++ Mugiwara-Level3/Exercise2.go
@@ -0,0 +1,12 @@
+//Print all capital letters thrice with unicode
+package main
+
+import "fmt"
+
+func main() {
+	for i:= 65; i<=90; i++ {
+		for j:=0; j<3; j++ {
+			fmt.Printf("%#U\n", i)
+		}
+	}
+}
