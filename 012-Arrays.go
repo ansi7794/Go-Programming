32ba12fe51f4477c538c5f85335350ff0f16b9db
ansi7794 <anushree@bu.edu>
2018-10-30 01:18:25 -0400

    arrays

--- 012-Arrays.go
+++ 012-Arrays.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	//length of array is part of its type x and y are of different types
 	var x [5]int
+	var y [7]int
 	fmt.Println(x)
 	x[3] = 40
 	fmt.Println(x)
 	fmt.Println(len(x))
+	fmt.Println(y)
+	y[3] = 40
+	fmt.Println(y)
+	fmt.Println(len(y))
 }
