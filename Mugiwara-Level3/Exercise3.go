b0cfbafafedc94df54a5eaba68d43bc7f192b78c
ansi7794 <anushree@bu.edu>
2018-10-30 00:45:54 -0400

    exercise3

---
+++ Mugiwara-Level3/Exercise3.go
@@ -0,0 +1,13 @@
+//Print the years you have been alive using conditional for (like while loop)
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 1994
+	for x <= 2018 {
+		fmt.Println(x)
+		x++
+	}
+}
